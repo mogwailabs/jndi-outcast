d1ee1b795b33298300da784a03fbf5b185f3b9c3
Frederic Linn <fredericlinn@gmail.com>
2024-09-10 15:04:50 +0200

    Add module for Tomcat 9.x but JDK > 15 testing
    Because the ScriptEngineManager was removed, we now use JShell to execute our payload.

---
+++ tomcat-9-jshell/src/main/java/de/mogwailabs/tomcat9jshell/LookupServlet.java
@@ -0,0 +1,49 @@
+package de.mogwailabs.tomcat9jshell;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/lookup")
+public class LookupServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
+        System.setProperty("com.sun.jndi.ldap.object.trustSerialDat", "true");
+
+        String resourceName = request.getParameter("resource");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (resourceName == null || resourceName.isEmpty()) {
+            out.println("Error: Query parameter 'resource' is required.");
+            return;
+        }
+
+        try {
+            Object resource = InitialContext.doLookup(resourceName);
+
+            out.println("<h3>JNDI Lookup Result</h3>");
+            out.println("<p>Resource Name: " + resourceName + "</p>");
+            out.println("<p>Resource Value: " + resource.toString() + "</p>");
+        } catch (NamingException e) {
+            out.println("<h3>Error</h3>");
+            out.println("<p>Failed to perform JNDI lookup: " + e.getMessage() + "</p>");
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            out.println("<p><b>Stack Trace:</b><br>" + sw.toString().replaceAll("\n", "<br>") + "</p>");
+        } finally {
+            out.close();
+        }
+    }
+}
