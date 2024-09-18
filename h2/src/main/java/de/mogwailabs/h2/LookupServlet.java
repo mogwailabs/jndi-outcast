4ec14e7e7ccb916125ab96085a360521f0590864
Frederic Linn <fredericlinn@gmail.com>
2024-09-18 10:57:26 +0200

    Only respond with 200 if exploitation was successful

--- h2/src/main/java/de/mogwailabs/h2/LookupServlet.java
+++ h2/src/main/java/de/mogwailabs/h2/LookupServlet.java
@@ -15,13 +15,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/lookup")
 public class LookupServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String resourceName = request.getParameter("resource");
+        // We set the status code to 400 by default and only change it to 200 in case of exploitation success.
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        var resourceName = request.getParameter("resource");
 
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        var out = response.getWriter();
 
         if (resourceName == null || resourceName.isEmpty()) {
             out.println("Error: Query parameter 'resource' is required.");
@@ -29,8 +30,7 @@ public class LookupServlet extends HttpServlet {
         }
 
         try {
-            DataSource ds =  InitialContext.doLookup(resourceName);
-            ds.getConnection();
+            var resource = InitialContext.doLookup(resourceName);
 
             // Make sure the file has been created.
             Thread.sleep(100);
@@ -40,22 +40,21 @@ public class LookupServlet extends HttpServlet {
 
             out.println("<h3>JNDI Lookup Result</h3>");
             out.println("<p>Resource Name: " + resourceName + "</p>");
-            out.println("<p>Resource Value: " + ds + "</p>");
+            out.println("<p>Resource Value: " + resource.toString() + "</p>");
 
             if (file.exists()) {
-                // We don't need to set the status code to 200 - it's the default anyway.
+                response.setStatus(HttpServletResponse.SC_OK);
                 out.println("<p>EXPLOITATION SUCCESSFUL</p>");
                 file.delete();
             } else {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.println("<p>EXPLOITATION UNSUCCESSFUL</p>");
             }
-        } catch (NamingException | SQLException | InterruptedException e) {
+        } catch (NamingException | InterruptedException e) {
             out.println("<h3>Error</h3>");
             out.println("<p>Failed to perform JNDI lookup: " + e.getMessage() + "</p>");
 
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
+            var sw = new StringWriter();
+            var pw = new PrintWriter(sw);
             e.printStackTrace(pw);
 
             out.println("<p><b>Stack Trace:</b><br>" + sw.toString().replaceAll("\n", "<br>") + "</p>");
--- tomcat-10-nashorn/src/main/java/de/mogwailabs/tomcat10nashorn/LookupServlet.java
+++ tomcat-10-nashorn/src/main/java/de/mogwailabs/tomcat10nashorn/LookupServlet.java
@@ -12,13 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/lookup")
 public class LookupServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String resourceName = request.getParameter("resource");
+        // We set the status code to 400 by default and only change it to 200 in case of exploitation success.
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        var resourceName = request.getParameter("resource");
 
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        var out = response.getWriter();
 
         if (resourceName == null || resourceName.isEmpty()) {
             out.println("Error: Query parameter 'resource' is required.");
@@ -26,7 +27,7 @@ public class LookupServlet extends HttpServlet {
         }
 
         try {
-            Object resource = InitialContext.doLookup(resourceName);
+            var resource = InitialContext.doLookup(resourceName);
 
             // Make sure the file has been created.
             Thread.sleep(100);
@@ -39,19 +40,18 @@ public class LookupServlet extends HttpServlet {
             out.println("<p>Resource Value: " + resource.toString() + "</p>");
 
             if (file.exists()) {
-                // We don't need to set the status code to 200 - it's the default anyway.
+                response.setStatus(HttpServletResponse.SC_OK);
                 out.println("<p>EXPLOITATION SUCCESSFUL</p>");
                 file.delete();
             } else {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.println("<p>EXPLOITATION UNSUCCESSFUL</p>");
             }
         } catch (NamingException | InterruptedException e) {
             out.println("<h3>Error</h3>");
             out.println("<p>Failed to perform JNDI lookup: " + e.getMessage() + "</p>");
 
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
+            var sw = new StringWriter();
+            var pw = new PrintWriter(sw);
             e.printStackTrace(pw);
 
             out.println("<p><b>Stack Trace:</b><br>" + sw.toString().replaceAll("\n", "<br>") + "</p>");
