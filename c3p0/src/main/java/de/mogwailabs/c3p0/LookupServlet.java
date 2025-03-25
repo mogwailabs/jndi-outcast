960b255cfad2dfc2b6d5f6f9c3b909ac69f740ef
Frederic Linn <fredericlinn@gmail.com>
2025-03-25 11:58:52 +0100

    Add module for C3P0 testing

---
+++ c3p0/src/main/java/de/mogwailabs/c3p0/LookupServlet.java
@@ -0,0 +1,69 @@
+package de.mogwailabs.c3p0;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/lookup")
+public class LookupServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // We set the status code to 400 by default and only change it to 200 in case of exploitation success.
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        var resourceName = request.getParameter("resource");
+
+        response.setContentType("text/html");
+        var out = response.getWriter();
+
+        if (resourceName == null || resourceName.isEmpty()) {
+            out.println("Error: Query parameter 'resource' is required.");
+            return;
+        }
+
+        try {
+            DataSource dataSource = InitialContext.doLookup(resourceName);
+            dataSource.getConnection();
+
+        } catch (NamingException | SQLException e) {
+            out.println("<h3>Error</h3>");
+            out.println("<p>Failed to perform JNDI lookup: " + e.getMessage() + "</p>");
+
+            var sw = new StringWriter();
+            var pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            out.println("<p><b>Stack Trace:</b><br>" + sw.toString().replaceAll("\n", "<br>") + "</p>");
+        } finally {
+            // An exception is thrown no matter what, so we need to verify the existence of our created file in this block.
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            var filePath = "/usr/local/tomcat/temp/pwn.txt";
+            var file = new File(filePath);
+
+            out.println("<h3>JNDI Lookup Result</h3>");
+            out.println("<p>Resource Name: " + resourceName + "</p>");
+
+            if (file.exists()) {
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.println("<p>EXPLOITATION SUCCESSFUL</p>");
+                file.delete();
+            } else {
+                out.println("<p>EXPLOITATION UNSUCCESSFUL</p>");
+            }
+            out.close();
+        }
+    }
+}
