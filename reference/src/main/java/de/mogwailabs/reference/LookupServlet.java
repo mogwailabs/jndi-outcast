86f2b0838b84ac267586c89d26f2938ed1bbcd26
Frederic Linn <fredericlinn@gmail.com>
2025-03-25 15:15:12 +0100

    Add module for Reference testing

---
+++ reference/src/main/java/de/mogwailabs/reference/LookupServlet.java
@@ -0,0 +1,62 @@
+package de.mogwailabs.reference;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.naming.NamingException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/lookup")
+public class LookupServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // We set the status code to 400 by default and only change it to 200 in case of exploitation success.
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        var resourceName = request.getParameter("resource");
+
+        response.setContentType("text/html");
+        var out = response.getWriter();
+
+        if (resourceName == null || resourceName.isEmpty()) {
+            out.println("Error: Query parameter 'resource' is required.");
+            return;
+        }
+
+        try {
+            // This only works because we specified
+            // ENV CATALINA_OPTS="-Dcom.sun.jndi.ldap.object.trustURLCodebase=true
+            // in the Dockerfile.
+            Object resource = new javax.naming.InitialContext().lookup(resourceName);
+            // Make sure the file has been created.
+            Thread.sleep(1000);
+
+            var filePath = "/usr/local/tomcat/temp/pwn.txt";
+            var file = new File(filePath);
+
+            out.println("<h3>JNDI Lookup Result</h3>");
+            out.println("<p>Resource Name: " + resourceName + "</p>");
+
+            if (file.exists()) {
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.println("<p>EXPLOITATION SUCCESSFUL</p>");
+                file.delete();
+            } else {
+                out.println("<p>EXPLOITATION UNSUCCESSFUL</p>");
+            }
+        } catch (NamingException | InterruptedException e) {
+            out.println("<h3>Error</h3>");
+            out.println("<p>Failed to perform JNDI lookup: " + e.getMessage() + "</p>");
+
+            var sw = new StringWriter();
+            var pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            out.println("<p><b>Stack Trace:</b><br>" + sw.toString().replaceAll("\n", "<br>") + "</p>");
+        } finally {
+            out.close();
+        }
+    }
+}
